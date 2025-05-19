dac559783f946131a23c76e3e09160835ec393c6
Alasdair Rae <alasdairjohnrae@gmail.com>
2025-05-19 20:39:15 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WHSHandicapCalculator from './WHSHandicapCalculator.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WHSHandicapCalculator />
+  </React.StrictMode>
+);
